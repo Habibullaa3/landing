b5cd16d7a427289a2b5776f119932d04b3435f9a
Habibulla <habibullaa3@gmail.com>
2021-07-05 11:29:03 +0500

    third commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 This is the second project for Udacity Front End Developer Nanodegree
 - Navigation is built dynamically as an unordered list
+
--- js/app.js
+++ js/app.js
@@ -1,29 +1,41 @@
 const sections=document.querySelectorAll('section');
 const fragment=document.createDocumentFragment();
 
-function createNav(id, name){
+createNav=(id, name)=>{
     const itemHTML=`<a class="menu__link" data-id="${id}">${name}</a>`;
     return itemHTML;
 }
 
+isInViewport=(elem)=> {
+    const bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
 // build the nav
-function buildNavigation() {
+buildNavigation=() => {
     for(let i=0;i<sections.length; i++) {
         const newMenuItem=document.createElement('li');
         const sectionName=sections[i].getAttribute('data-nav');
         const sectionId=sections[i].getAttribute('id');
         newMenuItem.innerHTML=createNav(sectionId, sectionName);
         fragment.appendChild(newMenuItem);
-    //    console.log(sectionName)
     }
-   
+
     const navBarList=document.getElementById('navbar__list');
     navBarList.appendChild(fragment);
     
 }
 
+
+
 // Scroll to anchor ID using scrollTO event
-function scrollToElement(event){
+scrollToElement=(event)=>{
     if(event.target.nodeName === 'A'){
         const sectionId = event.target.getAttribute('data-id');
         const section = document.getElementById(sectionId);
@@ -38,7 +50,31 @@ navBarList.addEventListener('click', function(event){
 });
 // build menu
 buildNavigation();
-
-
+const menu=document.querySelectorAll("#navbar__list li");
+      menu[0].classList.add("home");
+      menu[1].classList.add("hotels");
+      menu[2].classList.add("contact");
+      menu[3].classList.add("footer");
+      console.log(menu);
+window.addEventListener('scroll', ()=>{
+    let current='';
+    sections.forEach(section => {
+        const sectionTop=section.offsetTop;
+        const sectionHeight=section.clientHeight;
+        if(pageYOffset>=(sectionTop-sectionHeight/3)) {
+            current=section.getAttribute('id');
+           
+        }
+        
+    });
+   
+    menu.forEach(li => {
+        li.classList.remove('active');
+        if(li.classList.contains(current)){
+            li.classList.add('active');
+        }
+       
+    });
+});
 
 
